2926d486fea8bac2c7d643852ef6043ed9d84d98
bradleyjosephh <bradleyjosephh@gmail.com>
2022-02-16 18:28:31 -0800

    update to date styling and font

--- src/components/Portfolio/PortfolioCard.js
+++ src/components/Portfolio/PortfolioCard.js
@@ -9,7 +9,7 @@ const PortfolioCard = (props) => {
   const { project } = props;
   return (
     <>
-      <Card color="dark" className="col-sm-3 mb-4 white-border">
+      <Card id="portcards" color="dark" className="col-sm-3 mb-4 white-border">
         <CardImg variant="top" src={project.img} />
         <CardBody>
           <CardTitle>{project.title}</CardTitle>
@@ -17,8 +17,8 @@ const PortfolioCard = (props) => {
             {project.description}
           </CardText>
         </CardBody>
-        <Button variant="primary" color="info" href={project.gitHub}>GitHub</Button>
-        <Button variant="primary" color="warning" href={project.liveSite}>Live View</Button>
+        <Button variant="primary" color="info" href={project.repo}>Repo</Button>
+        <Button variant="primary" color="warning" href={project.source}>Source</Button>
     </Card>
     <br />
     </>
